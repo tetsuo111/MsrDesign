39249e10082f8f9c1f6491f64eabfac8340546f6
tetsuo111 <tetsuochikazawa11@gmail.com>
2018-03-28 17:06:46 +0900

    ベンダープレフィックス

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var autoprefixer = require('gulp-autoprefixer');
 var sass = require('gulp-sass');
 
 //CSSファイル、javascriptファイルをミニマイズしてくれます。
@@ -13,6 +14,10 @@ gulp.task('uglify', function () {
 gulp.task('sass', function(){
 	gulp.src('css/sass/**/*.scss')
 		.pipe(sass({outputStyle: 'expanded'}))
+		.pipe(autoprefixer({
+		browsers: ['last 2 version', 'iOS >= 8.1', 'Android >= 4.4'],
+		cascade: false
+	}))
 		.pipe(gulp.dest('css/sass/'));
 });
 
